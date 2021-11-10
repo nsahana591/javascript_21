4351f7b740addf3ce79a6b095459eb44d807c21d
sahana591 <nsahana591@gmail.com>
2021-11-10 09:22:36 +0530

    main

---
+++ html/function/arraymethod/one.js
@@ -0,0 +1,8 @@
+let arr=[10,20,30];
+let new_arr=[...new Set(arr)];
+console.log(new_arr);
+console.log("_____****_____");
+let a=[1,2,3];
+a.forEach((sahana)=>{
+    console.log('hello');
+});
---
+++ html/function/two.js
@@ -0,0 +1,10 @@
+class A{
+    constructor(){
+        console.log('hello');
+    }
+    class B extends A{
+        constructor(){
+            console.log('hello,gm')
+        }
+    }
+}
